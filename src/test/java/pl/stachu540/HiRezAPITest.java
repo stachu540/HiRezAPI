b99e217f2cfc852dc02dde88255ead4e513b62be
Damian Staszewski <damian@stachuofficial.pl>
2017-07-19 13:21:56 +0200

    Second Try - First successful

--- src/test/java/pl/stachu540/HiRezAPITest.java
+++ src/test/java/pl/stachu540/HiRezAPITest.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.*;
 import pl.stachu540.hirezstudios.HiRezGames;
 import pl.stachu540.hirezstudios.Paladins;
 import pl.stachu540.hirezstudios.Smite;
+import pl.stachu540.util.StringData;
 
 import java.util.Arrays;
 import java.util.HashMap;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName("Hi-Rez API Test")
 class HiRezAPITest {
 
     static HiRezAPI api;
@@ -18,8 +23,8 @@ class HiRezAPITest {
         String dev_id = System.getenv("DEV_ID");
         String auth_key = System.getenv("AUTH_KEY");
         if (!dev_id.isEmpty() || !auth_key.isEmpty()) api = new HiRezAPI(dev_id, auth_key);
-        else if (dev_id.isEmpty()) throw new Error("Developer ID is empty.");
-        else if (auth_key.isEmpty()) throw new Error("Authorization Key empty.");
+        else if (dev_id.isEmpty()) fail(new Error("Developer ID is empty."));
+        else if (auth_key.isEmpty()) fail("Authorization Key empty.");
         Arrays.asList(Smite.Platform.values()).forEach(platform -> games.put("SMITE_" + platform.name(), api.smite(platform)));
         Arrays.asList(Paladins.Platform.values()).forEach(platform -> games.put("PALADINS_" + platform.name(), api.paladins(platform)));
     }
@@ -33,7 +38,9 @@ class HiRezAPITest {
     @DisplayName("Ping Test")
     void pingTesting() {
         games.forEach((s, api_data) -> {
-            api_data.ping();
+            String data = api_data.ping();
+            System.out.println(data);
+            assertTrue(data.indexOf("Ping successful.") != -1);
         });
     }
 
@@ -41,7 +48,9 @@ class HiRezAPITest {
     @DisplayName("Session Test")
     void sessionTesting() {
         games.forEach((s, api_data) -> {
-            api_data.testSession();
+            api_data.createSession();
+            StringData data = api_data.testSession();
+            System.out.println(data.toJsonArray());
         });
     }
 
@@ -49,7 +58,8 @@ class HiRezAPITest {
     @DisplayName("List Patches Test")
     void patchesTesting() {
         games.forEach((s, api_data) -> {
-            api_data.getPatchInfo();
+            StringData data = api_data.getPatchInfo();
+            System.out.println(data.toJsonArray());
         });
     }
 
