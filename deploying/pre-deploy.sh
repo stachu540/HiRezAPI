0b8f2ed67574cff645e2b5ae2192758f6bc2217b
Damian Staszewski <damian@stachuofficial.pl>
2017-07-20 03:03:55 +0200

    a fixing missing file

--- deploying/pre-deploy.sh
+++ deploying/pre-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_b20503c2b955_key -iv $encrypted_b20503c2b955_iv -in hirezapi.asc.enc -out hirezapi.asc -d
-gpg --fast-import hirezapi.asc
+openssl aes-256-cbc -K $encrypted_b20503c2b955_key -iv $encrypted_b20503c2b955_iv -in $TRAVIS_BUILD_DIR/deploying/hirezapi.asc.enc -out $TRAVIS_BUILD_DIR/deploying/hirezapi.asc -d
+gpg --fast-import $TRAVIS_BUILD_DIR/deploying/hirezapi.asc
