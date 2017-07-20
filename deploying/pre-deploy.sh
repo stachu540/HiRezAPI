da0b482e962b6828e6a6d0e09a890fca1c9af154
Damian Staszewski <damian@stachuofficial.pl>
2017-07-20 14:47:59 +0200

    Deploy keys changes.

--- deploying/pre-deploy.sh
+++ deploying/pre-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_b20503c2b955_key -iv $encrypted_b20503c2b955_iv -in $TRAVIS_BUILD_DIR/deploying/hirezapi.asc.enc -out $TRAVIS_BUILD_DIR/deploying/hirezapi.asc -d
-gpg --fast-import $TRAVIS_BUILD_DIR/deploying/hirezapi.asc
+openssl aes-256-cbc -K $encrypted_a15d794d685b_key -iv $encrypted_a15d794d685b_iv -in $TRAVIS_BUILD_DIR/deploying/deploy.asc.enc -out $TRAVIS_BUILD_DIR/deploying/deploy.asc -d
+gpg --fast-import $TRAVIS_BUILD_DIR/deploying/deploy.asc
