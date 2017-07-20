d360580dbd61f1ca277fa843150b3813d32838be
Damian Staszewski <damian@stachuofficial.pl>
2017-07-20 17:23:53 +0200

    deploying release is now only master branch

--- deploying/deploy.sh
+++ deploying/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 REPO_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-if [[ ! -z $TRAVIS_TAG  ]] && [[ $REPO_VERSION != *-SNAPSHOT ]] && [[ "$TRAVIS_BRANCH" != "dev" ]]; then
+if [[ ! -z $TRAVIS_TAG  ]] && [[ $REPO_VERSION != *-SNAPSHOT ]] && [[ "$TRAVIS_BRANCH" == 'master' ]]; then
     mvn deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deploying/settings.xml
 elif  [[ $REPO_VERSION == *-SNAPSHOT ]] && [[ "$TRAVIS_BRANCH" == 'dev' ]]; then
     mvn deploy -P sign,build-extras --settings $TRAVIS_BUILD_DIR/deploying/settings.xml
