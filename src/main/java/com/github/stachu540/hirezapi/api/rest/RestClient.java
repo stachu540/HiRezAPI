a3677a9e0ce128f2473a4b5103bdc3c4420bcd2a
Damian Staszewski <damian@stachuofficial.pl>
2017-10-22 20:38:29 +0200

    moved to constructor

--- src/main/java/com/github/stachu540/hirezapi/api/rest/RestClient.java
+++ src/main/java/com/github/stachu540/hirezapi/api/rest/RestClient.java
@@ -8,9 +8,10 @@ public class RestClient {
     private final RestTemplate rest = new RestTemplate();
     private final Authentication authentication;
 
-    { rest.setErrorHandler(new RestErrorHandler()); }
-
-    public RestClient(Authentication authentication) {this.authentication = authentication;}
+    public RestClient(Authentication authentication) {
+        this.authentication = authentication;
+        this.rest.setErrorHandler(new RestErrorHandler());
+    }
 
     public <O extends Object> O request(String endpoint, Class<O> model, String... args) {
         String url = authentication.getUrl(endpoint, args);
