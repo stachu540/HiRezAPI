6fe3fc2e6fd16fe15af5fc000b50f0f21f5ee4c3
Damian Staszewski <damian@stachuofficial.pl>
2018-06-22 09:15:58 +0200

    feat(USER): F-R-I-EN-D-S

--- common/src/main/java/hirezapi/endpoints/UserEndpoint.java
+++ common/src/main/java/hirezapi/endpoints/UserEndpoint.java
@@ -3,7 +3,11 @@ package hirezapi.endpoints;
 import hirezapi.HiRezApi;
 import hirezapi.json.Achievements;
 import hirezapi.json.Player;
+import hirezapi.json.Friend;
 import hirezapi.json.PlayerStatus;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UserEndpoint extends AbstractEndpoint {
   public UserEndpoint(HiRezApi api) {
@@ -23,5 +27,13 @@ public class UserEndpoint extends AbstractEndpoint {
   public PlayerStatus getPlayerStatus(String user) {
     return api.getRestController()
           .request(buildUrl("getplayerstatus", user), PlayerStatus[].class)[0];
+  }
+
+  public List<Friend> getFriends(String user) {
+    if (!api.getConfiguration().getPlatform().getPlatform().equalsIgnoreCase("PC")) {
+      throw new UnsupportedOperationException("Friend list is supported only PC platform.");
+    }
+    return Arrays.asList(api.getRestController()
+          .request(buildUrl("getfriends", user), Friend[].class));
   }
 }
---
+++ common/src/main/java/hirezapi/json/Friend.java
@@ -0,0 +1,23 @@
+package hirezapi.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Friend extends Model {
+  private Long playerId;
+  private Long accountId;
+  private String avatarUrl;
+  private String name;
+}
